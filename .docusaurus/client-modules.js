7a2bc2d5d33961b9fd977094cc34f38c819a009a
louiscklaw <louiscklaw@gmail.com>
2022-07-06 19:20:21 +0800

    update helloworld,

--- .docusaurus/client-modules.js
+++
@@ -1,8 +0,0 @@
-export default [
-  require("/home/logic/_del/render-examples-docusaurus/node_modules/infima/dist/css/default/default.css"),
-  require("/home/logic/_del/render-examples-docusaurus/node_modules/@docusaurus/theme-classic/lib/prism-include-languages"),
-  require("/home/logic/_del/render-examples-docusaurus/node_modules/@docusaurus/theme-classic/lib/admonitions.css"),
-  require("/home/logic/_del/render-examples-docusaurus/node_modules/@docusaurus/theme-classic/lib/nprogress"),
-  require("/home/logic/_del/render-examples-docusaurus/src/css/custom.css"),
-  require("/home/logic/_del/render-examples-docusaurus/node_modules/plugin-image-zoom/src/zoom"),
-];
